9a51457261eea8edfb7d53ad4b80f8224cc7836d
ronald-onesta <professional.burgundy@gmail.com>
2024-02-04 15:09:22 -0600

    cleanup

--- src/components/subscription/StripePricingTable.tsx
+++ src/components/subscription/StripePricingTable.tsx
@@ -14,9 +14,8 @@ const StripePricingTable = () => {
     };
   }, []);
   return React.createElement("stripe-pricing-table", {
-    "pricing-table-id": "prctbl_1OgCflCLPADkTljcIdzPukni",
-    "publishable-key":
-      "pk_test_51NyS5wCLPADkTljcNsxH5B71sfFMfC1t47MFQv3JAcFWnV0yVBcfV6hvhR18igcbz1Y0IG79EtCA3vXoZ9Vjax6W008Q95NrMj",
+    "pricing-table-id": "<your-pricing-table-id>",
+    "publishable-key": "<your-stripe-publishable-key>",
   });
 };
 export default StripePricingTable;
