54c70c96b2cc821d458a2eb08763b8bfe68bd16d
ronald-onesta <professional.burgundy@gmail.com>
2024-08-12 09:36:33 -0500

    fix build error with password reset page

--- src/app/password-reset/page.tsx
+++ src/app/password-reset/page.tsx
@@ -1,60 +1,12 @@
 "use client";
 
-import { auth } from "@/lib/firebase/firebaseClient";
-import { confirmPasswordReset, verifyPasswordResetCode } from "firebase/auth";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
+import PasswordResetForm from "@/components/auth/PasswordResetForm";
+import { Suspense } from "react";
 
 export default function PasswordResetPage() {
-  const [newPassword, setNewPassword] = useState("");
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const oobCode = searchParams.get("oobCode");
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  const handlePasswordReset = async () => {
-    if (!oobCode) {
-      toast.error("Error resetting password: invalid code.");
-      return;
-    }
-
-    try {
-      // Verify the password reset code is valid
-      await verifyPasswordResetCode(auth, oobCode as string);
-      // If valid, update the password
-      await confirmPasswordReset(auth, oobCode as string, newPassword);
-      toast.success("Password has been reset successfully!");
-      router.push("/"); // Redirect to login page
-    } catch (error) {
-      toast.error("Failed to reset password. Please try again.");
-    }
-  };
-
-  if (!isClient) return null;
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white shadow-lg">
-        <h1 className="text-xl font-bold text-center">Reset your password</h1>
-        <input
-          type="password"
-          className="input input-bordered w-full"
-          placeholder="New Password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-        />
-        <button
-          onClick={handlePasswordReset}
-          className="btn btn-primary w-full"
-        >
-          Reset Password
-        </button>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PasswordResetForm />
+    </Suspense>
   );
 }
